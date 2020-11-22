56075271f5146d2769607258f5dc00c3d68d34d4
Nathan Strobbe <nathan.strobbe@gmail.com>
2020-11-22 18:07:16 +0100

    Change config, validation checks and hashing algorithm

--- sharex-api.php
+++ sharex-api.php
@@ -3,12 +3,12 @@
 /*
   ********************
   ShareX-API
-  By Senator
+  By Senator Modified by Nathan Strobbe
   ********************
   
   MIT License
 
-  Copyright (c) 2016 senator
+  Copyright (c) 2020 Nathan Strobbe
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
@@ -32,18 +32,18 @@
 // NOTE: REMEMBER TO SET upload_max_filesize in the php.ini!
 
 // Set Our Return data type to text, (to prevent any sort of XSS or php shell execution exploitation)
-header("Content-Type: text/text");
+header('Content-Type: text/text');
 
 ///
-/// Application Paramaters
+/// Application Parameters
 ///
 $config = array();
 
-$config["key"] = ""; // This is the master key, to access the uploader
-$config["save"] = "i/"; // Enter the directory to save to (keep empty for none)
-$config["host"] = "http://" . $_SERVER['HTTP_HOST'] . "/";
-$config["allowed"] = array("png", "jpg", "gif", "rar", "zip", "mp4", "mp3", "txt", "h", "cpp", "lua", "dll");
-$config["max_upload_size"] = 25; // IN MB
+$config['key'] = getenv('API_KEY'); // This is the master key, to access the uploader
+$config['save'] = 'i/'; // Enter the directory to save to (keep empty for none)
+$config['host'] = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+$config['allowed'] = array('png', 'jpg', 'gif', 'rar', 'zip', 'mp4', 'mp3', 'txt');
+$config['max_upload_size'] = 25; // IN MB
 
 ///
 /// Upload File
@@ -51,43 +51,42 @@ $config["max_upload_size"] = 25; // IN MB
 function UploadFile($config)
 {
 
-  // Validate Key
-  if(!isset($_POST["key"]) || $_POST["key"] != $config["key"])
-    die("INVALID_KEY");
+    // Validate Key
+    if (!isset($_POST['key']) || $_POST['key'] != $config['key']) {
+        die(401);
+    }
 
-  // Validate ShareX
-  if(!isset($_FILES["fdata"]))
-    die("INVALID_DATA_PACKET");
+    // Validate ShareX
+    if (!isset($_FILES['fdata'])) {
+        die('INVALID_DATA_PACKET');
+    }
 
-  if($_FILES["fdata"]["size"] > $config["max_upload_size"] * 1024 * 1024)
-    die("DATA_TOO_LARGE");
+    if ($_FILES['fdata']['size'] > $config['max_upload_size'] * 1024 * 1024) {
+        die('DATA_TOO_LARGE');
+    }
 
-  // Create Data for file
-  $data = array();
-  $data["filename"] = $_FILES["fdata"]['name'];
-  $data["buffer"] = $_FILES["fdata"]["tmp_name"];
-  $data["extension"] = pathinfo($_FILES["fdata"]['name'], PATHINFO_EXTENSION);
-  $data["final-save-name"] = $config["save"] . $data["filename"] . "." . $data["extension"];
-  //$data["uploaded"] = move_uploaded_file($data["buffer"], $data["final-save-name"]);
+    // Create Data for file
+    $data = array();
+    $data['filename'] = $_FILES['fdata']['name'];
+    $data['buffer'] = $_FILES['fdata']['tmp_name'];
+    $data['extension'] = pathinfo($_FILES['fdata']['name'], PATHINFO_EXTENSION);
+    $data['final-save-name'] = $config['save'] . $data['filename'] . '.' . $data['extension'];
 
-  // Validate Extension
-  if(!in_array($data["extension"], $config["allowed"]))
-    die("INVALID_DATA_EXTENSION");
+    // Validate Extension
+    if (!in_array($data['extension'], $config['allowed'])) {
+        die('INVALID_DATA_EXTENSION');
+    }
 
-  if(move_uploaded_file($data["buffer"], $data["final-save-name"]))
-  {
-    $file_signed = crc32(md5_file($data["final-save-name"])) % 100000; // Sign file with a crc32 and md5'd file hash (Also good because they cant upload the same file twice)
-    rename($data["final-save-name"], $config["save"] . $file_signed . "." . $data["extension"]); // Rename file
+    if (move_uploaded_file($data['buffer'], $data['final-save-name'])) {
 
-    die($config["host"] . $config["save"] . $file_signed . "." . $data["extension"]); // Return file location
-  }
-  else
-  {
-    die("FILE_CANT_UPLOAD");
-  }
+        $file_signed = substr(md5(time() . rand() . $data['final-save-name']), 0, 10);
+        // TODO if file name exists regenerate name
+        rename($data['final-save-name'], $config['save'] . $file_signed . '.' . $data['extension']); // Rename file
 
-  die("FILE_ERROR_UNKNOWN");
+        die($config['host'] . $config['save'] . $file_signed . '.' . $data['extension']); // Return file location
+    }
 
+    die('FILE_CANT_UPLOAD');
 }
 
 UploadFile($config);
